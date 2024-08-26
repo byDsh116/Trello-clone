be520629a2b4b3f1d60ea8a46b4c61ad5d25521b
byDsh116 <by.dsh116@gmail.com>
2024-08-26 14:46:40 +0300

    add ConfigModule and SequelizeModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { Sequelize } from 'sequelize-typescript';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/user.model';
+import { TaskColumn } from './columns/column.model';
+import { Card } from './cards/card.model';
+import { Commentary } from './comments/commentary.model';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'dsh',
+      password: '123',
+      database: 'trello',
+      models: [User, TaskColumn, Card, Commentary],
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
