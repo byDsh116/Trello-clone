015f2db492da48ad3ead513b033d418082d5ccf0
byDsh116 <by.dsh116@gmail.com>
2024-08-27 14:59:36 +0300

    add card model

---
+++ server/src/cards/card.model.ts
@@ -0,0 +1,30 @@
+import { BelongsTo, Column, DataType, ForeignKey, HasMany, Model, Table } from "sequelize-typescript";
+import { TaskColumn } from "src/columns/column.model";
+import { Commentary } from "src/comments/commentary.model";
+
+@Table
+export class Card extends Model<Card> {
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    title: string
+
+    @Column(DataType.TEXT)
+    description: string;
+
+
+    @ForeignKey(() => TaskColumn)
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    columnId: number;
+
+    @BelongsTo(() => TaskColumn)
+    column: TaskColumn
+
+    @HasMany(() => Commentary)
+    comments: Commentary[]
+
+}
