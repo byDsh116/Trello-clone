206422e80e4993603af0c6d78489202d3561afc8
byDsh116 <by.dsh116@gmail.com>
2024-08-31 10:50:32 +0300

    add modules: user, commentary,card,task

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { Sequelize } from 'sequelize-typescript';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './users/user.model';
 import { TaskColumn } from './columns/column.model';
 import { Card } from './cards/card.model';
-import { Commentary } from './comments/commentary.model';
+import { UsersModule } from './users/users.module';
+import { TaskColumnModule } from './columns/columns.module';
+import { CardModule } from './cards/cards.module';
+import { Commentary } from './commentary/commentary.model';
+import { CommentaryModule } from './commentary/commentary.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { Commentary } from './comments/commentary.model';
       autoLoadModels: true,
       synchronize: true,
     }),
+    UsersModule, TaskColumnModule, CardModule, CommentaryModule
   ],
   controllers: [],
   providers: [],
