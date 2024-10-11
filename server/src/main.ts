22daa090035b43ecc764e2cc69cb3b0b532ed9fc
byDsh116 <by.dsh116@gmail.com>
2024-10-11 13:06:03 +0300

    total fakap with swagger, commits before returning to the working version

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,35 @@
+
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   const config = new DocumentBuilder()
+//     .setTitle('Trello-clone API')
+//     .setDescription('API documentation for trello-clone application')
+//     .build()
+
+//   const document = SwaggerModule.createDocument(app, config)
+//   SwaggerModule.setup('api', app, document)
+
+//   app.enableCors()
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Trello-clone API')
-    .setDescription('API documentation for trello-clone application')
-    .build()
-
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
 
   app.enableCors()
   await app.listen(3000);
+  console.log('SERVER STARTED ON PORT 3000')
 }
+
 bootstrap();
